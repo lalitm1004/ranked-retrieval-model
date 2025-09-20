37429b5328bfd7184f1163e89a69e6dffc628b15
lalitm1004 <lalitm1004@gmail.com>
2025-09-20 22:10:01 +0530

    feat: add query vectorizer

---
+++ src/document_vectorizer.py
@@ -0,0 +1,76 @@
+import math
+from pathlib import Path
+from pydantic import BaseModel
+from typing import List, Dict
+
+from preprocessing import ProcessedDocument, Posting
+
+
+class DocumentTFVector(BaseModel):
+    doc_id: int
+    tf_vector: List[float]
+
+
+class DocumentTFVectorizer:
+    def __init__(
+        self,
+        processed_documents: List[ProcessedDocument],
+        posting_list: Dict[str, Posting],
+    ) -> None:
+        self.vectors: List[DocumentTFVector] = []
+
+        for doc in processed_documents:
+            tf_vector = [0.0] * len(posting_list)
+
+            for token in doc.tokens:
+                if token not in posting_list.keys():
+                    continue
+
+                token_index = posting_list[token].token_id
+
+                t_f = 0.0
+                for doc_id, freq in posting_list[token].postings:
+                    if doc_id == doc.doc_id:
+                        t_f = freq
+                        break
+
+                if t_f > 0.0:
+                    tf_vector[token_index] = 1 + math.log10(t_f)
+
+            norm = math.sqrt(sum(val * val for val in tf_vector))
+            if norm > 0:
+                tf_vector = [val / norm for val in tf_vector]
+
+            self.vectors.append(
+                DocumentTFVector(doc_id=doc.doc_id, tf_vector=tf_vector)
+            )
+
+    def save_vectors(self, output_path: Path) -> None:
+        with open(output_path, "w") as f:
+            for v in self.vectors:
+                f.write(v.model_dump_json(indent=4))
+
+
+if __name__ == "__main__":
+    from preprocessing import PreprocessingFactory
+
+    raw_path = Path("./data/raw")
+    processed_path = Path("./data/processed")
+    processed_path.mkdir(parents=True, exist_ok=True)
+
+    factory = PreprocessingFactory(docs_path=raw_path, proc_docs_path=processed_path)
+
+    # print("Processed Documents:")
+    # for doc in factory.docs:
+    #     print(
+    #         f"Doc ID: {doc.doc_id}, File: {doc.file_path.name}, Tokens: {doc.tokens[:10]}"
+    #     )
+
+    # print("\nPosting List:")
+    # for posting in factory.posting_list:
+    #     print(
+    #         f"Token ID: {posting.token_id}, Token: {posting.token}, Postings: {posting.postings}"
+    #     )
+
+    vectorizer = DocumentTFVectorizer(factory.docs, factory.posting_list)
+    vectorizer.save_vectors(Path("./data/processed/vectors.jsonl"))
